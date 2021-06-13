4b00a0ac6bb7589bba2b25745e21ab6289a4e704
manual-hue <h_0315@naver.com>
2021-06-13 23:09:33 +0900

    210613

--- java-basic/app/src/main/java/practicegw.java
+++ java-basic/app/src/main/java/practicegw.java
@@ -1,8 +1,21 @@
 package com.eomcs.lang.ex01;
 
+//  \n - Line Feed(LF), 0x0a
+//  \r - Carrage Return(CR), 0x0d
+//  \f - Form Feed, 0x0c
+//  \t - Tab, 0x09
+//  \b - Backspace, 0x08
+//  \' - Single Quote, 0x27
+//  \" - Double Quote, 0x22
+//  \\ - Backslash, 0x5c
+
 class gilbertwilliam {
     public static void main(String[] args) {
        System.out.println("Hello, Dear my G.P!");
        System.out.println("Hello, Dear my W.B!");
+       System.out.println(0.0314e2); // 0.0314 * 10의2승 = 3.14
+       System.out.println(0.314e1); // 0.314 * 10의1승 = 3.14
+       System.out.println(31.4e-1); // 31.4 * 10의-1승 = 3.14
+       System.out.println(314e-2); // 314 * 10의-2승 = 3.14
     }
 }
