ab024f031711dd7f8e8a9a2cc26b306b6d6d1c07
manual-hue <h_0315@naver.com>
2021-06-22 20:48:03 +0900

    0622

--- java-project-01/app/src-01/main/java/com/eomcs/App.java
+++ java-project-01/app/src-01/main/java/com/eomcs/App.java
@@ -1,6 +1,6 @@
-package com.eomcs.oop.overview.step01;
+package com.eomcs;
 
-//01. main(): 프로그램 entry point
+// main(): 프로그램 entry point
 public class App {
   public static void main(String[] args) {
     System.out.println("[게시판 관리]");
---
+++ java-project-01/app/src/main/java/com/eomcs/App.java
@@ -0,0 +1,14 @@
+package com.eomcs;
+
+import java.util.Date;
+import java.util.Scanner;
+
+
+public class App {
+  
+ public static void main(String[] args) {
+   System.out.println(":: 게시판 관리 ::");
+ }
+  
+  
+}
