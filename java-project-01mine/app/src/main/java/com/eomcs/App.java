374dd794d9541b52f12154f008a9963909cf8790
manual-hue <h_0315@naver.com>
2021-06-24 11:59:22 +0900

    0624

---
+++ java-project-01mine/app/src/main/java/com/eomcs/App.java
@@ -0,0 +1,43 @@
+package com.eomcs;
+
+import java.util.Scanner;
+
+public class App {
+
+  static Scanner keyScan = new Scanner(System.in);
+
+  public static void main(String[] args) {
+
+    // App 클래스의 Scanner 인스턴스를 각각 핸들러와 함께 쓴다.
+    BoardHandler.keyScan = keyScan;
+    MemberHandler.keyScan = keyScan;
+    
+    menuLoop: while(true) {
+      System.out.println("[메뉴]");
+      System.out.println("  1. 게시글 관리");
+      System.out.println("  2. 회원 관리");
+      System.out.print("메뉴를 선택하세요. (종료: quit) [1..2] ");
+      String menuNo = keyScan.nextLine();
+      
+      switch (menuNo) {
+        case "1":
+          BoardHandler.execute();
+          break;
+        case "2":
+          MemberHandler.service();
+          break;
+        case "quit":
+          break menuLoop;
+        default:
+          System.out.println("메뉴 번호가 옳지 않습니다.");
+      }
+     
+      System.out.println( );
+      
+    }
+    
+      keyScan.close();      
+      System.out.println("이용해주셔서 감사합니다.");
+      
+    }
+  }
---
+++ java-project-01mine/app/src/main/java/com/eomcs/MemberHandler.java
@@ -0,0 +1,29 @@
+package com.eomcs;
+
+import java.util.Scanner;
+
+public class MemberHandler {
+  
+  static Scanner keyScan;
+  
+  static void service() {
+    loop: while (true) {
+      System.out.println("[회원 관리]"); 
+      String command = keyScan.nextLine();
+      
+       switch (command) {
+         case "list": BoardHandler.list(); break;
+         case "add": BoardHandler.add(); break;
+         case "update": BoardHandler.update(); break;
+         case "delete": BoardHandler.delete(); break;
+         case "view": BoardHandler.view(); break;
+         case "back":
+           break loop;
+         default:
+           System.out.println("지원하지 않는 명령입니다.");
+         }
+     
+         System.out.println( );  
+      }
+    }
+  }
