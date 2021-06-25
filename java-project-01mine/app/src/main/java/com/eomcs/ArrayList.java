86e1272c0e537e59c9fed5b63d0e760aa4c69af6
manual-hue <h_0315@naver.com>
2021-06-25 15:21:25 +0900

    0625

--- java-project-01mine/app/src/main/java/com/eomcs/ArrayList.java
+++ java-project-01mine/app/src/main/java/com/eomcs/ArrayList.java
@@ -2,31 +2,38 @@ package com.eomcs;
 
 public class ArrayList {
 
+//  배열의 최대 크기를 지정하는 변수는 모든 인스턴스가 같다.
+//  따라서 각 인스턴스마다 이 변수를 가질 필요는 없다.
+//  그러니 이 변수는 그냥 클래스 변수로 둔다.
+//  보통 상수 변수, 즉, 파이널 변수는 클래스 변수(스태틱 변수)로 선언한다.
   static final int MAX_LENGTH = 100;
-  static Object[] list = new Object[MAX_LENGTH];
-  static int size = 0;
-
-  static void append(Object obj) {
-    list[size++] = obj;
+  
+//  각 인스턴스마다 개별적으로 관리해야 하는 변수는 인스턴스 변수로 선언한다.
+  Object[] list = new Object[MAX_LENGTH];
+  int size = 0;
+  
+//  메서드가 작업할 때 인스턴스 변수를 사용할 수 있도록 파라미터로 인스턴스 주소를 전달한다.
+  static void append(ArrayList that, Object obj) {
+    that.list[that.size++] = obj;
   }
 
-  static Object[] toArray() {
-    Object[] arr = new Object[size];
-    for (int i = 0; i < size; i++) {
-      arr[i] = list[i];
+  static Object[] toArray(ArrayList that) {
+    Object[] arr = new Object[that.size];
+    for (int i = 0; i < that.size; i++) {
+      arr[i] = that.list[i];
     }
     return arr;
   }
 
-  static Object retrieve(int index) {
-    return list[index];
+  static Object retrieve(ArrayList that, int index) {
+    return that.list[index];
   }
 
-  static void remove(int index) {
-    for (int i = index; i < size - 1; i++) {
-      list[i] = list[i + 1];
+  static void remove(ArrayList that, int index) {
+    for (int i = index; i < that.size - 1; i++) {
+      that.list[i] = that.list[i + 1];
     }
-    size--;
+    that.size--;
   }
 
 }
